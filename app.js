e4fb89c8c048d50f946af7fc413570ffa199a7fd
Abdul Wahab Raza <abdulwahabraza123@gmail.com>
2022-08-20 17:16:33 +0500

    last commit

--- app.js
+++ app.js
@@ -1,62 +1,28 @@
 const express=require('express');
 const path=require('path');
 const app=express();
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200})); 
 const port=process.env.PORT||8000;
 const staticPath=path.join(__dirname,'./public');
 app.use(express.static(staticPath));
-app.get("/api/timestamp/", function(req, res) {
+app.get("/api/", function(req, res) {
     var resDate = new Date();
     res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
   });
-app.get("/api/timestamp/:date?", function(req, res) {
-    var reqString = req.params.date;
-    var resDate;
-    if (!/^\d{4}-/.test(reqString)) reqString = parseInt(reqString);
-    resDate = new Date(reqString);
-    if (resDate.getTime() !== resDate.getTime()) {
-      res.json({ error: "Invalid Date" });
+app.get("/api/:date_string", function(req, res) {
+    resDate = new Date(req.params.date_string);
+    // console.log("This is time ",resDate.getTime());
+    if(resDate.toString() == "Invalid Date"){
+        resDate = new Date(parseInt(req.params.date_string));
     }
+    if(resDate.toString() == "Invalid Date"){
+        resDate=new Date();
+                // console.log(resDate);
+                res.json({ error : "Invalid Date" });
+              }
     res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
   });
-
-
-
-
-//   app.get('/api/timestamp',(req,res)=>{
-//     let date=new Date();
-//     console.log("Before date ",date);
-//     date.setMinutes(date.getMinutes()-3);
-//     res.json({
-//         'unix': date.getTime(), 
-//     'utc': date.toUTCString()
-//     }
-//     )
-// })
-// app.get("/api/timestamp/:date",(req,res)=>{
-//     let inputDate='';
-//     if (!/^\d{4}-/.test(req.params.date))
-//     {
-//          reqString = parseInt(reqString);
-//          console.log(reqString);
-//     }
-//     inputDate=new Date(req.params.date);
-    
-//     if(inputDate.toString() == "Invalid Date"){
-//         inputDate = new Date(parseInt(req.params.date));
-//       }
-//       if(inputDate.toString() == "Invalid Date"){
-//         inputDate=new Date().now;
-//         console.log(inputDate);
-//         res.json({ error : "Invalid Date" });
-//       }
-//       else{
-//     inputDate={
-//         'unix': inputDate.getTime(), 
-//     'utc': inputDate.toUTCString()
-//     }
-// }
-//     res.json(inputDate);
-// })
 app.listen(port,()=>{
     console.log("Listening to this port ",port);
 })
